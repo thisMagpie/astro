296620416f432077269b3b3fe7eeeed0e41d567b
Magdalen Berns <thisMagpie@live.com>
2013-10-12 02:11:59 +0100

    tidy up

--- python/euler_table.py
+++ python/euler_table.py
@@ -7,14 +7,14 @@ max_iters = 2
 def print_scheme_table():
     step_size  = [1.0, 0.1, 0.01, 0.001]
 
-    print "      {0} ".format(
-          "---------------------------------------------------------")
+    print "{0} ".format(
+          "-------------------------------------------------------------------")
 
-    print "     {0}          {1}        {2}".format(
-          " |    Forward "," Backward ","  Central      |")
+    print "{0}         {1}            {2}         {3}".format(
+          "|  Step", "Forward","Backward","  Central     |")
 
-    print "      {0} ".format(
-          "---------------------------------------------------------")
+    print "{0} ".format(
+          "-------------------------------------------------------------------")
 
     for j in range(len(step_size)):
 
@@ -28,22 +28,23 @@ def print_scheme_table():
 
         for i in range(max_iters):
             fn_forward[i]            =    1.0 / math.exp(forward(i, step_size[j]))
-            dfdx_forward             =    1.0 * first_diff(fn_forward[i - 1], fn_forward[i], step_size[j])
+            dfdx_forward             =    diff(fn_forward[i - 1], fn_forward[i], step_size[j])
 
             fn_backward[i]           =    1.0 / math.exp(backward(i, step_size[j]))
-            dfdx_backward            =    first_diff(fn_backward[i - 1], fn_backward[i], step_size[j])
+            dfdx_backward            =    diff(fn_backward[i - 1], fn_backward[i], step_size[j])
 
-            dfdx_central             =    0.5 * first_diff(fn_forward[i-1],  fn_backward[i] , step_size[j])
+            dfdx_central             =    0.5 * diff(fn_forward[i - 1], fn_backward[i] , step_size[j])
 
             if i != 0:
-                print "      {0} {1}       {2}      {3} {4}".format(
+                print "{0} {1}        {2}      {3}     {4}  {5}".format(
                                                         "|",
+                                                        float(step_size[j]),
                                                         float(dfdx_forward),
                                                         float(dfdx_backward),
                                                         float(dfdx_central),
                                                         "|")
     print "      {0} ".format(
-          "--------------------------------------------------------")
+          "-------------------------------------------------------------------")
 
 def forward(i, step):
     return step * (i - 1)
@@ -54,7 +55,7 @@ def backward(i, step):
 ''' @first_diff()  Works out the first differential for Taylor expansion
                    for function f[x],
     @return        double value of (f[i - 1] - f[i]) / step_size[j] '''
-def first_diff(f1, f0, step):
+def diff(f1, f0, step):
     return (f1 - f0) / step
 
 print_scheme_table()
